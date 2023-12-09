191f2248bdb862d73658e17b163204428c887a50
Manuel Arango <juan12292004@outlook.com>
2023-12-09 18:14:34 -0500

    implementación de solución ingenua

--- app/src/main/scala/taller4/Taller4.scala
+++ app/src/main/scala/taller4/Taller4.scala
@@ -11,14 +11,38 @@ import org.scalameter.Warmer
 
 object Taller4{
 
-  def saludo() = "Taller 4 2023-II"
-
   def main(args: Array[String]): Unit = {
-    println(saludo())
-    println(
-      withWarmer(new Warmer.Default) measure {
-        (1 to 100000000).toArray
-      }
-    )
+      
+      val s: List[String] = List("A","C","T","G")
+      println(PRC_ingenuo(s,13,oraculo))
   }
- }
+
+  def oraculo(sub: String,cad: String): Boolean = {
+    cad.contains(sub)
+  }
+
+  def cerradura(lenguaje: List[String], n: Int): List[String] = {
+    require(n >= 0)
+
+
+    def concatenar(c: List[String], m: Int): List[String] = {
+      if (m == 0) List("") 
+      else
+        for {
+          cadena <- c
+          resto <- concatenar(c, m - 1)
+        } yield cadena + resto
+    }
+
+    concatenar(lenguaje, n)
+  }
+
+   def PRC_ingenuo(L: List[String],n: Int,f:(String,String) => Boolean): String = {
+
+    var s = ""
+    for (i <- cerradura(L,n)){
+      if (f(i,"ATTTGATGACTAG")) s += i
+    }
+    s
+  }
+}
