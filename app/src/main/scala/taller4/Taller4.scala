7c963fe67745e55c7c7bf978bb99adbbea754761
Manuel Arango <juan12292004@outlook.com>
2023-12-12 11:56:18 -0500

    implementación de solución mejorada

--- app/src/main/scala/taller4/Taller4.scala
+++ app/src/main/scala/taller4/Taller4.scala
@@ -15,9 +15,19 @@ object Taller4{
       
       val s: List[String] = List("A","C","T","G")
       val oraculo = new oraculo(3)
+      println(cerradura(s, 1))
       println(oraculo.S)
-      println(PRC_ingenuo(s, 3, oraculo))
-     // println(cerradura(s, 3))
+      //println(PRC_ingenuo(s, 10, oraculo))
+      //println(PRC_mejorado(s, 10, oraculo))
+      val k = withWarmer(new Warmer.Default) measure {
+        PRC_ingenuo(s, 3, oraculo)
+      }
+      val w = withWarmer(new Warmer.Default) measure {
+        PRC_mejorado(s, 3, oraculo)
+      }
+      println(k)
+      println(w)
+        
   }
 
   def cerradura(l: List[String], n: Int): List[String] = {
@@ -40,5 +50,19 @@ object Taller4{
     val s = cerradura(L,n)
     val res = s.filter(ora.EsSubcadena(_))
     res.mkString
+  }
+  
+  def PRC_mejorado(L: List[String],n: Int, ora: oraculo): String = {
+    require(n > 0)
+    def PRC_mejorado_aux(L: List[String],res: List[String],n: Int, ora: oraculo, tam: Int): List[String] = {
+        val s = cerradura(L,tam)
+        val filtrado = s.filter(ora.EsSubcadena(_))
+        if (tam > n) res
+        else PRC_mejorado_aux(L,res++filtrado,n,ora,tam+1)
+    }
+    val w = PRC_mejorado_aux(L,List(),n,ora,1)
+    val z = w.length - 1
+    w(z)
+      
   }
 }
--- app/src/main/scala/taller4/oraculo.scala
+++ app/src/main/scala/taller4/oraculo.scala
@@ -1,13 +1,16 @@
 package taller4
 
 class oraculo(tam: Int){
-  val n: Int = tam
+  private val n: Int = tam
   val S: String = generarCadena(n)
   def EsSubcadena(sub: String): Boolean = {
     S.contains(sub)
   }
   def EsLaCadena(cad: String): String = {
     if (S == cad) S else ""
+  }
+  def getTamaño(): Int = {
+    n  
   }
   private def generarCadena (n: Int): String={
     (for(i <- 1 to n) yield scala.util.Random.nextInt(4) match {
